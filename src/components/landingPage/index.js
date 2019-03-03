c2f44ece4454b753080af96bc9b99f1b169b1d5a
Cade Holmes <holmesc16@gmail.com>
2019-03-03 00:44:57 -0600

    logo redo

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { withStateHandlers } from 'recompose'
 
 import {
-	StyledH1,
-	StyledValueProposition,
-	StyledSearchBar,
-	StyledSearchButton,
-	StyledSearchHelper
+  StyledH1,
+  StyledValueProposition,
+  StyledSearchBar,
+  StyledSearchButton,
+  StyledSearchHelper,
 } from './styles'
 
 import StyledContent from 'app/components/ui/Container'
@@ -19,51 +19,54 @@ import Row from 'app/components/ui/Row'
 import Logo from 'app/assets/logo.svg'
 
 const valuePropositionIconOptions = {
-	primary: colors.secondary[3],
-	secondary: colors.secondary[6],
-	width: '2.5rem'
+  primary: colors.secondary[3],
+  secondary: colors.secondary[6],
+  width: '2.5rem',
 }
 
 const LandingPage = ({ isActive, setIsActive }) => {
-	console.log({ isActive })
+  console.log({ isActive })
 
-	return (
-		<StyledContent>
-			<img src={Logo}/>
-			<StyledValueProposition>
-				<li>
-					<FactoryIcon {...valuePropositionIconOptions} />
-					Quickly generate boilerplate for your Javascript projects
-				</li>
-				<li>
-					<UserGroupIcon {...valuePropositionIconOptions} />
-					Discover templates made by developers all around the world
-				</li>
-				<li>
-					<PackageIcon {...valuePropositionIconOptions} />
-					Create your own templates to quickly scaffold future projects
-				</li>
-			</StyledValueProposition>
-			<Row style={{position: "relative"}}>
-				<StyledSearchBar onFocus={() => setIsActive(true)} onBlur={() => setIsActive(false)} placeholder="Discover templates now!" />
-				<StyledSearchButton>
-					<SearchIcon
-						width="2.5rem"
-						primary={'white'}
-						secondary={'none'}
-					/>
-				</StyledSearchButton>
-				<StyledSearchHelper isActive={isActive}>search examples: "react, vue, _lodash" "progressive web app" "username01-react"</StyledSearchHelper>
-			</Row>
-		</StyledContent>
-	)
+  return (
+    <StyledContent>
+      <img src={Logo} style={{ marginBottom: '40px', marginTop: '-20px' }} />
+      <StyledValueProposition>
+        <li>
+          <FactoryIcon {...valuePropositionIconOptions} />
+          Quickly generate boilerplate for your Javascript projects
+        </li>
+        <li>
+          <UserGroupIcon {...valuePropositionIconOptions} />
+          Discover templates made by developers all around the world
+        </li>
+        <li>
+          <PackageIcon {...valuePropositionIconOptions} />
+          Create your own templates to quickly scaffold future projects
+        </li>
+      </StyledValueProposition>
+      <Row style={{ position: 'relative' }}>
+        <StyledSearchBar
+          onFocus={() => setIsActive(true)}
+          onBlur={() => setIsActive(false)}
+          placeholder="Discover templates now!"
+        />
+        <StyledSearchButton>
+          <SearchIcon width="2.5rem" primary={'white'} secondary={'none'} />
+        </StyledSearchButton>
+        <StyledSearchHelper isActive={isActive}>
+          search examples: "react, vue, _lodash" "progressive web app"
+          "username01-react"
+        </StyledSearchHelper>
+      </Row>
+    </StyledContent>
+  )
 }
 
 export default withStateHandlers(
-	{ isActive: false },
-	{
-		setIsActive: () => (isActive) => ({
-			isActive
-		})
-	}
+  { isActive: false },
+  {
+    setIsActive: () => isActive => ({
+      isActive,
+    }),
+  }
 )(LandingPage)
